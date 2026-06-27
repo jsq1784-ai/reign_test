745556474822986fa49da7e378629a3b467c611c
jsq1784-ai <jsq1784-ai>
2026-06-27 17:33:12 -0500

    fix: espaciado corregido segun eslint

--- src/screens/home/index.tsx
+++ src/screens/home/index.tsx
@@ -3,14 +3,15 @@ import { Tabs } from "../../components/tabs"
 const Home = () : React.ReactElement => {
 
   return(
-      <div className="container">
-        <div className="row">
-          <p style={ {textAlign: "center", fontWeight: "bold", padding: "10px"} }>
+    <div className="container">
+      <div className="row">
+        <p style={ { textAlign: "center", fontWeight: "bold", padding: "10px" } }>
           Integrantes: Joseph Segovia | Rojas Jefferson | Abraham Ccopa
         </p>
-          <Tabs />
-        </div>
-      </div> 
+        <Tabs />
+      </div>
+    </div>
   )
 }
+
 export { Home }
