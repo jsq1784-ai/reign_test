c8fd16e185918aa2ef19b60d55e701a23631d36b
jsq1784-ai <jsq1784-ai>
2026-06-27 17:16:26 -0500

    feat: agregar nombres de integrantes

--- src/screens/home/index.tsx
+++ src/screens/home/index.tsx
@@ -5,6 +5,9 @@ const Home = () : React.ReactElement => {
   return(
       <div className="container">
         <div className="row">
+          <p style={{textAlign: "center", fontWeight: "bold", padding: "10px"}}>
+          Integrantes: Joseph Segovia | Rojas Jefferson | Abraham Ccopa
+        </p>
           <Tabs />
         </div>
       </div> 
